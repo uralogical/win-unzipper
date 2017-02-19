bab8cb013d9b0126689dd2de09110a839213f2da
uralogical <anny_padme@yahoo.com>
2017-02-19 21:38:10 +0900

    Move unzipped folder to parent directory

--- unzip.ps1
+++ unzip.ps1
@@ -6,4 +6,6 @@ foreach ($file in (dir *.zip)){
 	
 	$dest = $shell.NameSpace((Get-Item $dir).fullname)
 	$dest.CopyHere($zip.Items()) 
+    $path = Split-Path(Split-Path (Get-Item $dir).fullname -Parent) -Parent
+    Move-Item (Get-Item $dir).fullname $path
 }
